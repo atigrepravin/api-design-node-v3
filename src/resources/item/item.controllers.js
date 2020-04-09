1a2706ab854e7834e76887660193bd02f84cca3c
Pravin Atigre <atigrepravin@gmail.com>
2020-04-09 18:30:06 +0530

    api routing

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,20 @@
-export default {}
+const getMany = (req, res) => {
+  res.send({ item: 'getMany' })
+}
+
+const createOne = (req, res) => {
+  res.send({ item: 'createOne' })
+}
+
+const getOne = (req, res) => {
+  res.send({ item: 'getOne' })
+}
+
+const updateOne = (req, res) => {
+  res.send({ item: 'updateOne' })
+}
+
+const deleteOne = (req, res) => {
+  res.send({ item: 'deleteOne' })
+}
+export default { getMany, createOne, getOne, updateOne, deleteOne }
